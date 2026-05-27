af9e9dda0b265e48d8258a811b6ea85e2a0f7c72
Edgar-64 <edgarmaia.dev@gmail.com>
2026-05-27 15:39:46 -0300

    Add userGetAll and userDelete functions

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,4 +1,11 @@
+// src/services/user.ts
 import { apiRequest } from "./api";
+
+// Busca os dados de um usuário específico ou a listagem geral se o backend suportar
+export const userGetAll = () => apiRequest("GET", "/users");
+
+// Deleta um usuário do sistema
+export const userDelete = (userId: string | number) => apiRequest("DELETE", `/users/${userId}`);
 
 export const userGet = (userId: string | number) => apiRequest("GET", `/users/${userId}`);
 export const userPut = (dados: any, userId: string | number) => apiRequest("PUT", `/users/${userId}`, dados);
